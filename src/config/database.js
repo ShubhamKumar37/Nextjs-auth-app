04207fe6e59a708591d0d101c0d26a9c738c4430
ShubhamKumar37 <sk9818281820@gmail.com>
2024-08-20 21:30:31 +0530

    first commit on nextjs auth app

---
+++ src/config/database.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+export async function dbConnect()
+{
+    try{
+        mongoose.connect(process.env.DATABASE_URL);
+
+        const connection = mongoose.connection;
+
+        connection.on("connected", () =>
+        {
+            console.log("Database is connected successfully ");
+        });
+
+        connection.on("error", (Error) =>
+        {
+            console.log("Make sure that the database is up ");
+            console.log("This is the error of not running database ->", Error);
+            process.exit(1);
+        })
+    }
+    catch(Error)
+    {
+        console.log("DB is not connected");
+        console.log("This is the error for no connected with database = ", Error);
+    }
+}
---
+++ src/models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            require: [true, "Please provide a usename"],
+            unique: true
+        },
+        email: {
+            type: String,
+            require: [true, "Please provide a email"],
+            unique: true
+        },
+        password: {
+            type: String,
+            require: [true, "Please provide a password"],
+        },
+        userName: {
+            type: String,
+            require: [true, "Please provide a usename"],
+            unique: true
+        },
+        isVerified:{
+            type: Boolean,
+            default: false,
+        },
+        isAdmin:{
+            type:Boolean,
+            default: false,
+        },
+        forgotPasswordToken: String,
+        forgotPasswordTokenExpiry: Date,
+        verifyToken: String,
+        verifyTokenExpiry: Date,
+    }
+)
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
